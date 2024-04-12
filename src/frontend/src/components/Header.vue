5a90c2f56b7ce4c3393efab33be8d5dd494f6309
pettroch <sidoro-petya@bk.ru>
2024-04-12 16:04:56 +1000

    add Header

---
+++ src/frontend/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="flex justify-between px-20 py-6 border-b border-slate-300">
+        <div class="hover:-translate-y-0.5 hover:cursor-pointer">
+            <img src="/header.png" alt="logo" class="w-80 test" />
+            <p class="text-slate-700 text-right font-mono font-bold">ServiceDesk</p>
+        </div>
+                
+        <ul class="flex items-center gap-6">
+            <li class="flex items-center hover:cursor-pointer hover:-translate-y-0.5 hover:font-bold active:text-slate-600">
+            <img src="/create_task.svg" class="w-6" />
+            <p class="text-base font-mono">Создать заявку</p>
+            </li>
+
+            <li class="flex gap-1 items-center hover:cursor-pointer hover:-translate-y-0.5 hover:font-bold active:text-slate-600">
+            <img src="/my_tasks.svg" class="w-6"/>
+            <p class="text-base font-mono">Мои заявки</p>
+            </li>
+
+            <li class="flex items-center hover:cursor-pointer hover:-translate-y-0.5 hover:font-bold active:text-slate-600">
+            <img src="/exit.svg" class="w-6" />
+            <p class="text-base font-mono">Выход</p>
+            </li>
+        </ul>
+    </header>
+  </template>
